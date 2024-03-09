ee745af9d5ef886a810cfd6045d47add148af905
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-03-09 16:57:01 -0300

    Rota de Cadastro

--- src/main/java/br/com/projeto/api/controller/ClienteController.java
+++ src/main/java/br/com/projeto/api/controller/ClienteController.java
@@ -1,16 +1,33 @@
 package br.com.projeto.api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.api.models.Cliente;
+import br.com.projeto.api.repositories.ClienteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
 public class ClienteController {
 
-    
+    @Autowired
+    private ClienteRepository repository;
+
+    @PostMapping("/")
+    public Cliente cadastrar(@RequestBody Cliente c){
+        return repository.save(c);
+    }
+
     @GetMapping("/")
     public String test(){
         return "Hello World";
     }
+
+
     
 }
--- src/main/java/br/com/projeto/api/repositories/ClienteRepository.java
+++ src/main/java/br/com/projeto/api/repositories/ClienteRepository.java
@@ -1,11 +1,10 @@
 package br.com.projeto.api.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.projeto.api.models.Cliente;
 
-@Repository
+
 public interface ClienteRepository extends CrudRepository<Cliente, Long>{
     
 }
