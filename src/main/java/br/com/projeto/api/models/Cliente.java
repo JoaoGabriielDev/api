39101ad3dcb43fbfdf2740739756e3ea8048c3e6
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-03-09 16:45:57 -0300

    Configurando Entidade

--- src/main/java/br/com/projeto/api/models/Cliente.java
+++ src/main/java/br/com/projeto/api/models/Cliente.java
@@ -1,5 +1,36 @@
 package br.com.projeto.api.models;
 
-public class Cliente {
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_clientes")
+@Getter
+@Setter
+public class Cliente implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private int idade;
+
+    private String cidade;
+
+    public Cliente(){
+
+    }
+
+    
+
     
 }
